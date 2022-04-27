2fe917134c8c9651c5dc44ff34a1ff989018f09c
JPatel87 <jaime_patel@hotmail.co.uk>
2022-04-27 15:49:43 +0000

    deployment commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn locks_of_lush.wsgi
---
+++ locks_of_lush/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for locks_of_lush project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locks_of_lush.settings')
+
+application = get_wsgi_application()
