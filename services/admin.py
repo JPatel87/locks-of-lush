e33760e4ea92c3889f08c826944a4500d77c9d2f
JPatel87 <jaime_patel@hotmail.co.uk>
2022-05-01 20:56:54 +0000

    Address flake8 errors

--- services/admin.py
+++ services/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import Service
 
 # Register your models here.
-class ServiceAdmin(admin.ModelAdmin):  
+
+
+class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'service_type', 'price')
     list_filter = ('service_type',)
+
 
 admin.site.register(Service, ServiceAdmin)
--- services/models.py
+++ services/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Service(models.Model):
     class ServiceType(models.TextChoices):
         CUT = 'CUT', 'cut'
