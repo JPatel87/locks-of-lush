67384f6e0f89902b553e34fc8a6fbb9ae386bb36
JPatel87 <jaime_patel@hotmail.co.uk>
2022-05-15 13:31:32 +0000

    Add booking model to bookings app

---
+++ bookings/urls.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Booking(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    stylist = models.ForeignKey('stylists.Stylist', on_delete=models.CASCADE, default=2)
+    service = models.ForeignKey('services.Service', on_delete=models.CASCADE, default=1)
