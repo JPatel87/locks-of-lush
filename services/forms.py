216d7bd413451663b9ffe581f17041ec1cb31da3
JPatel87 <jaime_patel@hotmail.co.uk>
2022-06-07 14:01:38 +0000

    Add labels for service form

--- services/forms.py
+++ services/forms.py
@@ -5,6 +5,10 @@ class ServiceForm(forms.ModelForm):
     class Meta: 
         model = Service
         fields = ['name', 'service_type', 'price']
+        labels = {
+            'name': 'Service name',
+            'price': 'Price (£)'
+        }
 
     def clean_name(self):
         return self.cleaned_data['name'].capitalize()
