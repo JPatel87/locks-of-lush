823dd18db6ec53bcff6ce16a059350d9fff46c6f
JPatel87 <jaime_patel@hotmail.co.uk>
2022-06-07 20:08:46 +0000

    Add placeholder widget for services price

--- services/forms.py
+++ services/forms.py
@@ -9,6 +9,9 @@ class ServiceForm(forms.ModelForm):
             'name': 'Service name',
             'price': 'Price (£)'
         }
+        widgets = {
+            'price': forms.TextInput(attrs={'placeholder': 'e.g £99.99 enter as 99.99'}),
+        }
 
     def clean_name(self):
         return self.cleaned_data['name'].capitalize()
