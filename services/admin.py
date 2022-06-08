fe2370ce06f6959623bd35fbfb491178c6bdac63
JPatel87 <jaime_patel@hotmail.co.uk>
2022-06-08 14:37:40 +0000

    Add docstrings to services admin

--- services/admin.py
+++ services/admin.py
@@ -1,12 +1,16 @@
+"""Imports from django and models."""
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
 from .models import Service
 
 # Register your models here.
 
 
-class ServiceAdmin(admin.ModelAdmin):
+@admin.register(Service)
+class ServiceAdmin(ModelAdmin):
+    """
+    Service model view on django admin for admin user.
+    """
+
     list_display = ('name', 'service_type', 'price')
     list_filter = ('service_type',)
-
-
-admin.site.register(Service, ServiceAdmin)
