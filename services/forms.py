5be6dfb9b32e0239833d3866a159af7c57dcff24
JPatel87 <jaime_patel@hotmail.co.uk>
2022-06-08 13:46:39 +0000

    Add docstrings to service form

--- services/forms.py
+++ services/forms.py
@@ -1,15 +1,27 @@
+"""Imports from django and models."""
 from django import forms
 from .models import Service
 
+
 class ServiceForm(forms.ModelForm):
-    class Meta: 
+    """
+    Service form used to add services.
+    """
+
+    class Meta:
+        """
+        Class to define service form fields and labels.
+        """
+
         model = Service
         fields = ['name', 'service_type', 'price']
         labels = {
             'name': 'Service name',
             'price': 'Price (£)'
         }
-        
-    def clean_name(self):
-        return self.cleaned_data['name'].capitalize()
 
+    def clean_name(self):
+        """
+        Method to capitalize service names from service form.
+        """
+        return self.cleaned_data['name'].capitalize()
