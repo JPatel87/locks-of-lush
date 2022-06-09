d292f3f580f0d8f05faeccc921c8a092ce07f32b
JPatel87 <jaime_patel@hotmail.co.uk>
2022-06-09 13:08:59 +0000

    Add bookings urls

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,10 @@
+"""Imports from django and bookings views"""
 from django.urls import path
-from bookings.views import get_bookings_page
-
+from bookings import views
 
 urlpatterns = [
-    path('', get_bookings_page, name='bookings')
+    path('', views.bookings, name='bookings'),
+    path('add/', views.add_bookings, name='add_bookings'),
+    path('edit/<booking_id>', views.edit_bookings, name='edit_bookings'),
+    path('delete/<timeslot_id>', views.delete_bookings, name='delete_bookings')
 ]
