d931007b9c09dd1d62606d7a1c77759aeeb65697
JPatel87 <jaime_patel@hotmail.co.uk>
2022-06-09 12:27:44 +0000

    Add bookings admin model

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,6 +1,19 @@
+"""Imports from django and booking model."""
+from django.contrib.admin import ModelAdmin
 from django.contrib import admin
 from .models import Booking
 
 
-# Register your models here.
-admin.site.register(Booking)
+@admin.register(Booking)
+class BookingAdmin(ModelAdmin):
+    """
+    Booking model view on django admin for admin user.
+    """
+    list_display = (
+        'date',
+        'time',
+        'stylist',
+        'first_name',
+        'last_name',
+        'service'
+        )
