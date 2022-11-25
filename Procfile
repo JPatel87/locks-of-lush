6a3277d4c994e463cffdf714b6ce3a39133b8f93
JPatel87 <jaime_patel@hotmail.co.uk>
2022-11-25 21:05:46 +0000

    preparation for render deployment

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn locks_of_lush.wsgi
--- bookings/apps.py
+++ bookings/apps.py
@@ -1,6 +1,9 @@
+"""Imports from django"""
 from django.apps import AppConfig
 
 
 class BookingsConfig(AppConfig):
+    """Imports from django"""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookings'
